7368b9373bb9bba78ce6d1f192e160b4525fe5f8
Micah <micahand@live.unc.edu>
2019-03-18 17:45:11 -0400

    resource counter addition

---
+++ Multiple Canvases/Assets/R1.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R1 : MonoBehaviour
+{
+
+    private resource resourceScript;
+    private int count;
+    private SpriteRenderer resOne;
+
+
+    private void Awake()
+    {
+        resourceScript = GetComponent<resource>();
+        count = resourceScript.resourceCount;
+
+        resOne = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (count == 0)
+        {
+            resOne.color = new Color(1f, 1f, 1f, 0f);
+        }
+    }
+}
